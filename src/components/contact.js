0964d7ce6a333e763f12b6a43e499a24b3ecd442
Seha Gulnar Dalay <sehagulnar@gmail.com>
2018-04-09 15:03:28 -0400

    Added adressbook and contact js files

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,4 +2,16 @@ import React from 'react';
 
 import './contact.css';
 
-// The Contact component goes here.  It should be the default export.
+export default function Contact(props) {
+     return (
+         <div className="contact" id={`${props.index}`}>
+            <img className="contact-photo" src={props.photo} alt={`${props.name} photo`} />
+            <h2 className="contact-name">
+             {props.name} 
+            </h2>
+            <address className="contact-address">
+             {props.address}
+            </address>
+        </div>
+     )
+}
